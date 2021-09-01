db70cd041ef0f3a84783249a2fad338ef115a23f
Anton Konovalov <extempl@pm.me>
2021-09-01 15:32:34 +0300

    Fix parsing absent properties file

--- properties-format-parser.js
+++ properties-format-parser.js
@@ -3,7 +3,7 @@ const parser = require('properties-parser');
 const parseProperties = (data) => parser.parse(data);
 
 const updatePropertiesWithValues = (data, lang, keysToUpdate, platform) => {
-  const dataMap = parseProperties(data);
+  const dataMap = parseProperties(data || '');
   keysToUpdate.forEach(key => {
     dataMap[key.key_name[platform]] = key.translations.find(t => t.language_iso === lang).translation;
   })
